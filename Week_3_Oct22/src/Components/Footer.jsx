5233f3e24e15f093a2d304d1ab6d2be9bfb998f3
KishorekrD-cypher <kishorekrdeloitte@outlook.com>
2024-10-26 11:10:59 +0000

    Add files via upload

---
+++ Week_3_Oct22/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react"
+
+function Footer(){
+    return(
+        <p>My app inc.</p> 
+    )
+}
+
+export default Footer;
---
+++ Week_3_Oct22/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import ReactDOM from 'react-dom/client';
+
+function Home(){
+    return(
+        <>
+        <Header />
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
