f9273abf6b0661c81bc5897c00d91bbbee85e1d2
KishorekrD-cypher <kishorekrdeloitte@outlook.com>
2024-10-26 11:14:57 +0000

    Add files via upload

---
+++ Week_3_Oct23/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React,{ Component } from "react";
+import Footer from '../Components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from '../Components/Navbar';
+
+function Home(){
+    return(
+        <>
+        <Navbar />
+        <Footer />
+        </>
+    )
+}
+export default Home;
